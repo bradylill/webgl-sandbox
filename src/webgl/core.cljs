47f64212d8e465d4838eb2f357ef0433402d3119
Brady <bradylill@gmail.com>
2015-02-07 22:34:06 +0000

    Strange lumpy sphere via shaders.

--- src/webgl/core.cljs
+++ src/webgl/core.cljs
@@ -19,7 +19,7 @@
   (do
     (.setSize renderer window-width window-height)
     (.appendChild (.-body js/document) (.-domElement renderer))
-    (.set (.-position camera) 0 0 5)))
+    (.set (.-position camera) 0 0 100)))
 
 (defn init-lights! [scene]
   (do
@@ -30,57 +30,68 @@
 
 (def vertex-shader
   "
+  attribute float displacement;
   varying vec3 vNormal;
 
   void main ()  {
     vNormal = normal;
 
+    vec3 newPosition = position +
+                       normal +
+                       vec3(displacement);
+
     gl_Position = projectionMatrix *
                   modelViewMatrix *
-                  vec4(position, 1.0);
+                  vec4(newPosition, 1.0);
   }")
 
 (def fragment-shader
- "
- varying vec3 vNormal;
+  "
+  varying vec3 vNormal;
 
- void main ()  {
-   vec3 light = vec3(0.5, 0.2, 1.0);
-   light = normalize(light);
+  void main ()  {
+    vec3 light = vec3(0.5, 0.2, 1.0);
+    light = normalize(light);
 
-   float dProd = max(0.0, dot(vNormal, light));
+    float dProd = max(0.0, dot(vNormal, light));
 
-   gl_FragColor = vec4(dProd, dProd, dProd, 1.0);
- }")
+    gl_FragColor = vec4(dProd, dProd, dProd, 1.0);
+  }")
 
-(defn create-material []
-  (THREE.ShaderMaterial. #js
-                        {:vertexShader vertex-shader
-                         :fragmentShader fragment-shader}))
+(defn attributes [geo]
+  (let [values (for [v (.-vertices geo)] (* (rand) 5))]
+    {:displacement {:type "f"
+                    :value values}}))
 
-(defn create-box []
-  (let [geo       (THREE.BoxGeometry. 1 1 1)
-        material  (create-material)]
+(defn create-material [geo]
+  (THREE.ShaderMaterial. (clj->js
+                           {:attributes     (attributes geo)
+                            :vertexShader   vertex-shader
+                            :fragmentShader fragment-shader})))
+
+(defn create-sphere []
+  (let [geo       (THREE.SphereGeometry. 30 30 30)
+        material  (create-material geo)]
     (THREE.Mesh. geo material)))
 
-(defn add-box-to-scene! [scene]
-  (let [box (create-box)]
-    (.add scene box)
-    box))
+(defn add-sphere-to-scene! [scene]
+  (let [sphere (create-sphere)]
+    (.add scene sphere)
+    sphere))
 
 (defn init! [scene renderer camera]
   (init-three! renderer camera)
   (init-lights! scene))
 
-(defn draw [{:keys [x y box] :as world-state}]
-  (aset box "rotation" "x" x)
-  (aset box "rotation" "y" y)
+(defn draw [{:keys [x y sphere] :as world-state}]
+  (aset sphere "rotation" "x" x)
+  (aset sphere "rotation" "y" y)
   (.render renderer scene camera))
 
 (init! scene renderer camera)
 
 (big-bang!
-  :initial-state {:x 0 :y 0 :box (add-box-to-scene! scene)}
+  :initial-state {:x 0 :y 0 :sphere (add-sphere-to-scene! scene)}
   :on-tick
   (fn [event world-state]
     (-> world-state
