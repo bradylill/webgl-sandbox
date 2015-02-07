98d52dd3584e1f4c40e90c3628da7174534db733
Brady <bradylill@gmail.com>
2015-02-07 21:49:21 +0000

    Fake lighting!

--- src/webgl/core.cljs
+++ src/webgl/core.cljs
@@ -29,15 +29,28 @@
       (.add scene directional-light))))
 
 (def vertex-shader
-  "void main ()  {
-     gl_Position = projectionMatrix *
-                   modelViewMatrix *
-                   vec4 ( position, 1.0 );
+  "
+  varying vec3 vNormal;
+
+  void main ()  {
+    vNormal = normal;
+
+    gl_Position = projectionMatrix *
+                  modelViewMatrix *
+                  vec4(position, 1.0);
   }")
 
 (def fragment-shader
- "void main ()  {
-   gl_FragColor = vec4 (1.0, 0.0, 1.0, 1.0);
+ "
+ varying vec3 vNormal;
+
+ void main ()  {
+   vec3 light = vec3(0.5, 0.2, 1.0);
+   light = normalize(light);
+
+   float dProd = max(0.0, dot(vNormal, light));
+
+   gl_FragColor = vec4(dProd, dProd, dProd, 1.0);
  }")
 
 (defn create-material []
